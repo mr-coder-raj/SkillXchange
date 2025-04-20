e0f9bd4ad31f2937531616632ac28e104c953065
MrCoderRaj <rajr127655@gmail.com>
2025-04-20 18:08:50 +0530

    Initial commit

--- middleware.js
+++ middleware.js
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*), /sign-up(.*)', '/api/create'])
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)', 
+  '/sign-up(.*)', 
+  '/api/create'
+]);
+
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) await auth.protect()
